a6937dfdd56a2259564f7654f7e06a942d9fe01e
ssh-22 <r.march.13_liberty0@yahoo.ne.jp>
2019-06-21 14:54:01 +0900

    modified wsgi.py

--- csirt/settings.py
+++ csirt/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FILES_DIR = os.path.abspath(os.path.join(BASE_DIR, '../../../nvd'))
 PROJECT_NAME = os.path.basename(BASE_DIR)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -138,7 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Configuration in case of using django-allauth
@@ -172,9 +176,9 @@ ALLOWED_HOSTS = ['*']
 
 # STATIC_ROOT = 'staticfiles'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 
 DEBUG = False
 
