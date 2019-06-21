af6455617164efd617719e0c41a387208d239fec
ssh-22 <r.march.13_liberty0@yahoo.ne.jp>
2019-06-21 16:57:37 +0900

    modified settings.py

--- csirt/settings.py
+++ csirt/settings.py
@@ -172,7 +172,7 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'herokuapp.com']
 
 # STATIC_ROOT = 'staticfiles'
 
